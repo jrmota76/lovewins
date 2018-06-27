5d929257bbd3c788ea427dd9bea5365fc55ed237
jrmota76 <40637475+jrmota76@users.noreply.github.com>
2018-06-27 10:30:10 -0500

    Add files via upload

---
+++ jessica.js
@@ -0,0 +1,10 @@
+function conosolePrint(){
+    console.log("hello Jessica");
+  }
+   function alertPrint(){
+    alert("hello Jessica");
+  }
+   function documentPrint(){
+    document.write("hello Jessica")
+  }
+ 
